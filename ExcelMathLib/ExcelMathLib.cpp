de0a077f57ab78244c198aa20cedc76cf2ac4945
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-05-30 11:20:53 -0400

    Updated header file, preparing for first build.

--- ExcelMathLib/ExcelMathLib.cpp
+++ ExcelMathLib/ExcelMathLib.cpp
@@ -3,3 +3,29 @@
 #include <utility>
 #include <limits.h>
 #include "ExcelMathLib.h"
+double WINAPI get_square(double* x) //returns square of a given number
+{
+    return *x * *x;
+}
+
+double WINAPI add_nums(double* x, double* y) //adds two numbers together
+{
+    return *x + *y;
+}
+
+double WINAPI divide_nums(double* x, double* y) 
+//divides two numbers, with first input as dividend, and second number
+//is the divisor
+{
+    return *x / *y;
+}
+
+double WINAPI add_two(double* x) //adds 2.0 to the given input x
+{
+    return *x + 2.0;
+}
+
+double WINAPI power(double* x, double* y) // raises x to the power of y.
+{
+    return pow(*x, *y);
+}
--- ExcelMathLib/ExcelMathLib.h
+++ ExcelMathLib/ExcelMathLib.h
@@ -7,3 +7,8 @@
 #define EXCELMATHLIB_API __declspec(dllimport)
 #endif
 
+extern "C" EXCELMATHLIB_API double get_square(double* x);
+extern "C" EXCELMATHLIB_API double add_nums(double* x, double* y);
+extern "C" EXCELMATHLIB_API double divide_nums(double* x, double* y);
+extern "C" EXCELMATHLIB_API double add_two(double* x);
+extern "C" EXCELMATHLIB_API double power(double* x, double* y);
