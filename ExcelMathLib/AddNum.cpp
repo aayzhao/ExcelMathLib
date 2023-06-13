f785e2f30b2b035f89f438dc3863cbaef0d8fa55
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-06-13 12:44:00 -0400

    creating a c++  class

---
+++ ExcelMathLib/AddNum.cpp
@@ -0,0 +1,12 @@
+#include "AddNum.h"
+
+AddNum::AddNum()
+{
+	num1 = 0;
+	num2 = 0;
+}
+
+double AddNum::addNums()
+{
+	return num1 + num2;
+}
---
+++ ExcelMathLib/AddNum.h
@@ -0,0 +1,9 @@
+#pragma once
+class AddNum
+{
+public: 
+	double num1;
+	double num2;
+	AddNum();
+	double addNums();
+};
