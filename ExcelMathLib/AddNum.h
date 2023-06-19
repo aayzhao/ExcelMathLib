d46261a86c9c20dab697c8dfcafeb6cf3728d0a4
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-06-19 11:36:13 -0400

    C wrapper work 6/19/2023

--- ExcelMathLib/AddNum.h
+++ ExcelMathLib/AddNum.h
@@ -2,10 +2,11 @@
 class AddNum
 {
 public: 
-	double num1;
-	double num2;
 	AddNum();
 	AddNum(double x);
 	AddNum(double x, double y);
 	double addNums();
+private:
+	double num1;
+	double num2;
 };
