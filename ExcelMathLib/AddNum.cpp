362ae6cb9165d3979c1d0691a218f7bc47f61c4a
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-06-19 11:35:25 -0400

    Further C wrapper  work

--- ExcelMathLib/AddNum.cpp
+++ ExcelMathLib/AddNum.cpp
@@ -1,10 +1,22 @@
 #include "pch.h"
 #include "AddNum.h"
 
-AddNum::AddNum(int x)
+AddNum::AddNum()
+{
+	num1 = 0.0;
+	num2 = 0.0;
+}
+
+AddNum::AddNum(double x)
 {
 	num1 = 0.0 + x;
 	num2 = 0.0 + x;
+}
+
+AddNum::AddNum(double x, double y)
+{
+	num1 = 0.0 + x;
+	num2 = 0.0 + y;
 }
 
 double AddNum::addNums()
--- ExcelMathLib/AddNum.h
+++ ExcelMathLib/AddNum.h
@@ -4,6 +4,8 @@ class AddNum
 public: 
 	double num1;
 	double num2;
-	AddNum(int x);
+	AddNum();
+	AddNum(double x);
+	AddNum(double x, double y);
 	double addNums();
 };
